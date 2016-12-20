619372ed93c8f6fd38c5a1353a3415949c1342ef
zhangted <tedcbook@gmail.com>
2016-12-20 13:02:52 -0500

    Update register.php

--- register.php
+++ register.php
@@ -5,17 +5,39 @@
 session_start();
 include('connect.php');
 
-if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['register'])) {
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['register']) && !empty($_POST['usr']) && !empty($_POST['pw'])) {
 
-  //insert variables from register form into database
+  date_default_timezone_set('America/New_York');
+
+  //Variable creation
   $usr_insert = htmlspecialchars($_POST['usr_register']);
   $pw_strip = htmlspecialchars($_POST['pw_register']);
-  
   $pw_insert = pasword_hash($pw_strip, DEFAULT_PASSWORD);
+  $action_insert = 'New User';
+  $date_insert = date('m/d/Y h:i a', time());
 
+  //Check if username exists
+  $query = 'SELECT * FROM credentials WHERE usr = '".$usr_insert."'';
+  $check_q = mysqli_query($query);
+  if(mysqli_num_rows($check_q)) == 1) {
+
+    echo "<script type='text/javascript'>alert('Username already in database!');</script>";
+    header('Location: register.php');
+
+  }
+  else {
+
+  //Insert values into database
+  $query = 'INSERT into credentials (usr, pw, leftaction, rightaction, leftdatetime, rightdatetime)
+  VALUES ('".$usr_insert."', '".$pw_insert."', '".$action_insert."', '".$action_insert."', '".$date_insert."', '".$date_insert."')';
+  $run_query = mysqli_query($query);
+
+  }
 }
+
 else {
 
+  //Admin Reg. Form
   echo('Register an administrator through this form:<br />');
 
   echo('<form action='$_SERVER['PHP_SELF']' method='post'>
@@ -23,8 +45,8 @@ else {
         Desired password:<input type='password' placeholder='Password' name='pw_register'><br /><br />
         <input type='submit' placeholder='Register' name='register'>
         </form>');
+        
 }
-
 
 
 ?>
